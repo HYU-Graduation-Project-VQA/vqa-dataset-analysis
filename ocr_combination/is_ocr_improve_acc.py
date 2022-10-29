8190aa43ad764abf31b8c1c0942fdbd689c3d1e4
Gaon-Choi <choigaon1028@gmail.com>
2022-10-30 01:46:33 +0900

    OCR + combination Result

--- ocr_combination/is_ocr_improve_acc.py
+++ ocr_combination/is_ocr_improve_acc.py
@@ -6,7 +6,7 @@ QUESTION_JSON = 'val2014_json/v2_mscoco_val2014_annotations.json'
 ENTROPY_JSON = 'logit_entropy_analysis/val_banc1280_logit_epoch12.json'
 OCR_JSON = 'ocr_combination/ocr_results_combination.json'
 
-threshold = 3.0
+threshold = [5.0, 4.9, 4.8, 4.7, 4.6, 4.5, 4.4, 4.3, 4.2, 4.1, 4.0, 3.9, 3.8, 3.7, 3.6, 3.5, 3.4, 3.3, 3.2, 3.1, 3.0]
 
 def find_imgId(jsonfile, qid):
     # criteria : multiple_choice_answer
@@ -34,32 +34,40 @@ if __name__ == '__main__':
     with open(OCR_JSON) as f3:
         ocr_json = json.load(f3)
 
-    result_list = list()
+    for i in range(len(threshold)):
+        print(threshold[i])
+        result_list = list()
 
-    # 내 모델이 낸 답과 비교하기
-    for q in tqdm.tqdm(entropy_json):
-        isEntropyExceed = q['entropy'] >= threshold
-        if (isEntropyExceed is False):
-            continue
-        qid = q['question_id']
-        # image_id = find_imgId(question_json, qid)
-        image_id, answer = find_imgId(question_json, qid)
+        # 내 모델이 낸 답과 비교하기
+        for q in tqdm.tqdm(entropy_json):
+            isEntropyExceed = (q['entropy'] >= threshold[i])
+            if (isEntropyExceed is False):
+                continue
+            qid = q['question_id']
+            # image_id = find_imgId(question_json, qid)
 
-        # answer = q['answer']
-        ocr_answer = find_ocrAnswer(ocr_json, image_id)
-        isBlank = False
-        isExist = False
-        ratio = None
-        if not ocr_answer:
-            isBlank = True
+            # answer: multiple_choice_answer
+            image_id, answer = find_imgId(question_json, qid)
 
-        elif answer in ocr_answer:
-            isExist = True
-            ratio = float(1/len(ocr_answer))
+            mymodel_answer = q['answer']
+            isCorrect = (mymodel_answer == answer)
 
-        # ratio: 1 / 해당 image id에서 뽑은 OCR 텍스트 전체 개수
-        qdict = {'qid': qid, 'isblank': isBlank, 'isExist': isExist, 'answer': answer, 'ratio': ratio}
-        result_list.append(qdict)
-    
-    with open('ocr_combination/annotation_OCR_result.json', 'w') as fp:
+            ocr_answer = find_ocrAnswer(ocr_json, image_id)
+            isBlank = False
+            isExist = False
+            ratio = None
+
+            if not ocr_answer:
+                isBlank = True
+
+            elif answer in ocr_answer:
+                isExist = True
+                ratio = float(1/len(ocr_answer))
+
+            # ratio: 1 / 해당 image id에서 뽑은 OCR 텍스트 전체 개수
+            qdict = {'qid': qid, 'isblank': isBlank, 'isExist': isExist, 'answer': answer,
+            'model_answer': mymodel_answer, 'isCorrect': isCorrect, 'ratio': ratio}
+            result_list.append(qdict)
+        
+        with open('ocr_combination/annotation_OCR_result(thr={thr}).json'.format(thr=threshold[i]), 'w') as fp:
             json.dump(result_list, fp, indent=4)
